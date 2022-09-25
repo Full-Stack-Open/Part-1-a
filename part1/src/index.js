931e58ce25502dc92f53ae91c1ce8d5696c0be6f
Fernando Warno <ferwar@hotmail.com>
2022-09-25 06:12:36 -0300

    pasar props

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2.-El componente Hello recibe el props.name a traves de las (props)
+const Hello = (props) => {
+  return (
+    <>
+      <p>Hello {props.name}</p>
+      Hello {props.name}, you are {props.age} years old
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1.-En la etiqueta del componente Hello defino la propiedad y le asigno un valor.
+// En este caso la propiedad name es un string, y se lo paso al componente Hello.
+// Si el valor del prop se logra usando JavaScript, debe estar envuelto con llaves.
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="George" age="13"/>
+      <Hello name="Daisy" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root')) 
